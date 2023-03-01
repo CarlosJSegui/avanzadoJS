8cc968e640cb85949a7cc4f7ad00af87d8c1cc80
CarlosJ <cjaviersaguilar@gmail.com>
2023-03-01 15:19:09 -0500

    types in ts

--- testing/typescript/index.ts
+++ testing/typescript/index.ts
@@ -1,9 +1,61 @@
-console.log("Hello TypeScript");
+// console.log("Hello TypeScript");
 
-function add(a:number, b:number) {
-    return a + b;
+// function add(a:number, b:number) {
+//     return a + b;
+// }
+
+// const sum = add(2, 3);
+
+// console.log(sum)
+
+// Boolean
+let muted: boolean = true;
+
+//Números
+let age = 6
+let numerador: number = 42;
+let denominador: number = age;
+let resultado = numerador/denominador;
+
+//String
+let nombre: string = "Carlos";
+let saludo = "Me llamo " + nombre;
+
+// console.log(saludo);
+
+//Arreglos
+let people: string[] = [];
+people = ["Isabel", "NIcole", "Raul"];
+// people.push("9000")
+let peopleAndNumbers: Array < number| string > = [];
+peopleAndNumbers.push("Ricardo");
+peopleAndNumbers.push(9901);
+// console.log(peopleAndNumbers);
+
+// Enum
+enum Color {
+
+    Rojo = "Rojo",
+
+    Verde = "Verde",
+
+    Azul = "Azul",
+
+    Amarillo = "Amarillo",
 }
 
-const sum = add(2, 3);
+let colorFavorito: Color = Color.Amarillo;
+// console.log(`Mi color favorito es ${colorFavorito}`);
 
-console.log(sum)
+// Any
+
+let comodin: any = "Joker"
+comodin = {
+    type: "Wildcard",
+}
+
+// Object
+
+let someObject: object = {
+    type: "Wildcard",
+}
