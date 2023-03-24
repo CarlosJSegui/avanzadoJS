9ce7914750a2e921e559bfa96faa802efba5af35
Carlos Segui <cjaviersaguilar@gmail.com>
2023-03-24 02:38:38 -0400

    AutoPause

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,13 @@
 import MediaPlayer from "./MediaPlayer.js";
 import AutoPlay from "./plugins/Autoplay.js";
+import AutoPause from "./plugins/AutoPause.js";
 
 const video = document.querySelector ("video")
 const playPause = document.querySelector ("#playPause")
 const muteUnmute = document.querySelector ("#muteUnmute")
 const player = new MediaPlayer({
     el: video, 
-    plugins: [new AutoPlay()]
+    plugins: [new AutoPlay(), new AutoPause()],
 });
 
 playPause.onclick = () => player.togglePlay();
